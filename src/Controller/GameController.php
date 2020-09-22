b1f446b3917ae39e3dd17a61357db3abd59ee465
raphaelbacco <raphael@bacco.fr>
2020-09-22 21:56:47 +0200

    hall/candidat

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -88,6 +88,7 @@ class GameController extends AbstractController
 
     /**
      * @Route("/jeu/halls", name="game_halls")
+     * @Route("/jeu/candidats", name="game_candidats")
      * @param EntityManagerInterface $em
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
@@ -109,6 +110,7 @@ class GameController extends AbstractController
 
     /**
      * @Route("/jeu/halls/{name}", name="game_hall")
+     * @Route("/jeu/candidat/{name}", name="game_candidat")
      * @param Request $request
      * @param EntityManagerInterface $em
      * @param Hall $hall
