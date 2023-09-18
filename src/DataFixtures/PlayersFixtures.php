cf6c59fd82ec52bd34672baf65501594bfc92412
Raphaël Bacco <raphael@bacco.fr>
2023-09-18 16:32:15 +0200

    Fixtures GPDC 2023

--- src/DataFixtures/PlayersFixtures.php
+++ src/DataFixtures/PlayersFixtures.php
@@ -2,14 +2,18 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Hall;
 use App\Entity\Player;
-use App\Entity\Question;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class PlayersFixtures extends Fixture implements FixtureGroupInterface
 {
+    public static function getGroups(): array
+    {
+        return ['players'];
+    }
+
     public function load(ObjectManager $manager): void
     {
         $player = new Player();
@@ -61,23 +65,7 @@ class AppFixtures extends Fixture
         $player->setEmail("tom@cruise.com");
         $manager->persist($player);
 
-        for ($i=1; $i<10; $i++) {
-            $hall = new Hall();
-            $hall->setName($i);
-            $manager->persist($hall);
-
-            $question = new Question();
-            $question->setQuestion("Quelle est la bonne réponse à la question $i ?");
-            $question->setAnswers(implode("\n", [
-                "Réponse 1",
-                "Réponse 2",
-                "Réponse 3",
-                "Réponse 4",
-            ]));
-            $question->setHall($hall);
-            $manager->persist($question);
-        }
-
         $manager->flush();
     }
+
 }
